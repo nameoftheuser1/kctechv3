470b26417b0030aacfa0fcbfa75929c2711f85ee
nameoftheuser1 <angelo.jelo24@gmail.com>
2024-12-01 23:31:39 +0800

    edited footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -5,9 +5,16 @@
             <div class="w-1/2">
                 Experience comfort and luxury at our resort.
             </div>
-            <div class="w-1/2 flex">
-                <div class="w-1/2">Relax</div>
-                <div class="w-1/2">Reserve</div>
+            <div class="w-1/2 flex flex-col">
+                <div class="flex">
+                    <div class="w-1/2">Relax</div>
+                    <div class="w-1/2">Reserve</div>
+                </div>
+                <div class="mt-4">
+                    <p>Contact: Jesa Lejano</p>
+                    <p>Phone: 09057556578</p>
+                    <p>Email: kctech365@gmail.com</p>
+                </div>
             </div>
         </div>
     </div>
